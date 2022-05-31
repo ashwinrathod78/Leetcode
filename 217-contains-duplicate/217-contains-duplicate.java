c6d919a9377ca1f858ecbd733c05fb800ebd0b56
Ashwin Rathod <ashwinrathod78@gmail.com>
2022-06-01 01:37:05 +0530

    Time: 12 ms (53.44%), Space: 71.5 MB (18.68%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++) {
+            if(!set.add(nums[i]))
+                return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
