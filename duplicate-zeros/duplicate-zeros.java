9c1db77a1ced2e689baab0d2ce611a1cdf953df4
Ashwin Rathod <ashwinrathod78@gmail.com>
2022-06-04 13:53:03 +0530

    Time: 1 ms, Memory: 42.1 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.java
+++ duplicate-zeros/duplicate-zeros.java
@@ -1,6 +1,22 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        for(int i=1;i<arr.length;i++){
+       int left = 0, right=0;
+        for(;left < arr.length;left++) if(arr[left]==0) right++;
+        left=arr.length-1;
+        right = arr.length + right - 1;
+        while(left >= 0 && right >=0) {
+            if(arr[left]!=0) {
+                if(right < arr.length)
+                    arr[right] = arr[left];
+            } else {
+                if(right < arr.length)  arr[right] = arr[left];
+                    right--;
+                if(right < arr.length)  arr[right] = arr[left];
+            }
+            left --;
+            right --;
+        }
+        /* for(int i=1;i<arr.length;i++){
             if(arr[i-1]==0) {
                 for(int j=arr.length-1;j>i;j--) {
                     arr[j] = arr[j-1];
@@ -8,5 +24,6 @@ class Solution {
                 arr[i++]=0;
             }
         }
+        */
     }
 }
