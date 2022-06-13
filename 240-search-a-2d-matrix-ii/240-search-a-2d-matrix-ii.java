4aa107762e1135611f7a1611109aa3f115cb5a92
Ashwin Rathod <ashwinrathod78@gmail.com>
2022-06-14 00:33:57 +0530

    Time: 14 ms (23.57%), Space: 57.8 MB (45.89%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++) {
-            for(int j=0;j<matrix[i].length;j++) {
-                if(matrix[i][j] == target)
-                    return true;
-            }
+       if(matrix == null || matrix.length < 1 || matrix[0].length < 0)
+           return false;
+        int r = 0;
+        int c = matrix[0].length - 1;
+        while(c>=0 && r<matrix.length) {
+            if(matrix[r][c] == target)
+                return true;
+            else if(matrix[r][c] > target)
+                c--;
+            else
+                r++;
         }
         return false;
     }
